bb9f3428d240a811ae1cda57e0782d670ffe626a
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2018-09-26 11:07:46 -0400

    enemy movement

---
+++ 376 Game Dev/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    Transform Target;
+    [SerializeField]
+    float FollowSpeed;
+    [SerializeField]
+    float FollowRange;
+
+    void Update()
+    {
+        if (Target != null)
+        {
+            if (transform.position.x - Target.transform.position.x < FollowRange)
+            {
+                transform.LookAt(Target.position);
+                transform.Rotate(new Vector3(0, 90, 180), Space.Self);
+                transform.Translate(new Vector3(FollowSpeed * Time.deltaTime, 0, 0));
+
+            }
+        }
+    }
+}
