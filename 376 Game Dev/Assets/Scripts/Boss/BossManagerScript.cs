c05d17fbdd32daf70a64cdae3894de4434a9d16a
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2018-10-10 22:28:22 -0400

    - added random boss prefab holder for testing - created BossManagerScript with random spawning boss - created BossScalingScript for setting the level and number of players

---
+++ 376 Game Dev/Assets/Scripts/Boss/BossManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManagerScript : MonoBehaviour {
+
+    public GameObject[] bossList;
+
+    private GameObject currentBoss;
+    private int level;
+    private int players;
+
+	void Start () {
+        //get level and amount of players
+        //level = this.GetComponent<GameManagerScript>().GetLevel();
+        //players = this.GetComponent<GameManagerScript>().GeNumPlayer();
+
+        //instantiate random boss
+        currentBoss = Instantiate(bossList[Random.Range(0, bossList.Length)], transform.position, Quaternion.identity);
+        
+        //send information to boss created
+        currentBoss.GetComponent<BossScaleScript>().SetScale(level, players);
+
+    }
+	
+	void Update () {
+		//check if boss is dead, if so notify game manager
+        if(currentBoss == null)
+        {
+            //this.GetComponent<GameManagerScript>().BossDeath();
+        }
+	}
+}
