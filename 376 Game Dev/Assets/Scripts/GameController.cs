6e2de4f6a5d00992419dc01e64d7c070ba60dbd6
Catherine <hyerine0@gmail.com>
2018-10-31 18:21:12 -0400

    added spawn points on entry room, need to make players spawn on them

--- 376 Game Dev/Assets/Scripts/GameController.cs
+++ 376 Game Dev/Assets/Scripts/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : NetworkBehaviour
     private int numOfPlayers = 0;                           //Keep track of the number of players spawn
     private GameObject player;
     public float loadTime = 5.0f;
+    public Transform[] PlayerSpawn;
 
 
     //Awake is always called before any Start functions
