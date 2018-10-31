ff7ec44584322fabe8d74962c289d0825d363f2b
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2018-10-31 00:15:22 -0400

    - changed room 1 to initial room

---
+++ 376 Game Dev/Assets/Scripts/Map/BossSpawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawnManager : MonoBehaviour {
+
+    public float scale;
+    public float health;
+    public Vector2 spawnPoint;
+
+    private GameObject bossObject;
+
+    // Use this for initialization
+    void Start () {
+        //set a scale for the boss' health
+        //scale = level * numPlayer;
+
+        //multiply scale with base health
+        //health = scale * health;
+
+        //spawn random bossObject
+        bossObject = GameObject.Find("Manager").GetComponent<MapManagerScript>().getRandomBoss();
+        Instantiate(bossObject, spawnPoint, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //kill boss object when health is 0
+        /*
+        if(health <= 0)
+        {
+            Destroy(bossObject);
+        }
+        */
+		
+	}
+}
