7bf00acf6e2babf759855da98f24760833ecd206
Felix <FeelixBrault@gmail.com>
2018-11-03 18:08:51 -0400

    Added setPlayer function to WaveManager.cs Added when waves enemy array is empty, spawn random enemy from mapManager Added MainMenu.unity Added NetworkManager_Custom.cs for MainMenu.unity NetworkManager_Custom.cs now in MainMenu.unity (persists in MainScene.unity) In order to play from Unity start from MainMenu.unity

---
+++ 376 Game Dev/Assets/Scripts/NetworkManager_Custom.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class NetworkManager_Custom : NetworkManager {
+
+    public void startupHost()
+    {
+        if (!NetworkClient.active && !NetworkServer.active)
+        {
+            setPort();
+            NetworkManager.singleton.StartHost();
+        }
+    }
+
+    public void joinGame()
+    {
+        if (!NetworkClient.active && !NetworkServer.active)
+        {
+            setIPAddress();
+            setPort();
+            NetworkManager.singleton.StartClient();
+        }
+    }
+
+    void setIPAddress()
+    {
+        string ipAddress = GameObject.Find("InputFieldIPAddress").transform.Find("Text").GetComponent<Text>().text;
+        Debug.Log(ipAddress);
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+
+    void setPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 0)
+        {
+            StartCoroutine(setupMenuSceneButton());
+        }
+        else
+        {
+            setupOtherSceneButton();
+        }
+    }
+
+    IEnumerator setupMenuSceneButton()
+    {
+        yield return new WaitForSeconds(0.2f);
+        GameObject.Find("StartHostButton").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("StartHostButton").GetComponent<Button>().onClick.AddListener(startupHost);
+
+        GameObject.Find("JoinGameButton").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("JoinGameButton").GetComponent<Button>().onClick.AddListener(joinGame);
+    }
+
+    void setupOtherSceneButton()
+    {
+        GameObject.Find("DisconnectButton").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("DisconnectButton").GetComponent<Button>().onClick.AddListener(NetworkManager.singleton.StopHost);
+    }
+}
