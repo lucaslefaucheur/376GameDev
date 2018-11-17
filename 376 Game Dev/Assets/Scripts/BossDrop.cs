5fb1a1db7815cf8c10318898f244876d187e3cd5
Felix <FeelixBrault@gmail.com>
2018-11-16 19:34:07 -0500

    Added item folder for item drops and made script so boss now drop 1 random item

---
+++ 376 Game Dev/Assets/Scripts/BossDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BossDrop : NetworkBehaviour
+{
+
+    private List<GameObject> items = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        Object[] objList = Resources.LoadAll("Item", typeof(Object));
+
+        // Add object to map list
+        foreach (Object obj in objList)
+        {
+            GameObject item = (GameObject)obj;
+            items.Add(item);
+        }
+    }
+	
+    public void OnDestroy()
+    {
+        if (isServer)
+        {
+            //instantiate a single random item
+            GameObject itemPick = items[Random.Range(0, items.Count)];
+            GameObject itemDrop = Instantiate(itemPick, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            NetworkServer.Spawn(itemDrop);
+        }
+    }
+}
