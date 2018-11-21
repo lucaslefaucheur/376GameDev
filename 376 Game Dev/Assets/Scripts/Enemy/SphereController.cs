b51f6296ebf916962bad5e458a4b65c00834b94a
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2018-11-21 17:44:13 -0500

    the mines of the leopard explodes: finished

--- 376 Game Dev/Assets/Scripts/Enemy/SphereController.cs
+++ 376 Game Dev/Assets/Scripts/Enemy/SphereController.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class SphereController : MonoBehaviour {
 
+    private Animator anim;
+    private float counter; 
+
+    private void Start()
+    {
+        counter = 5.0f; 
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        Destroy(gameObject, 5);
+        if (counter > 0)
+            counter -= Time.deltaTime;
+        else {
+            counter = 5.0f;
+            anim.SetBool("Explodes", true);
+            Destroy(gameObject, 0.75f);
+        }
     }
 
     //Colliding with the player will cause damage to the player
@@ -15,6 +30,8 @@ public class SphereController : MonoBehaviour {
         if (other.gameObject.layer.Equals(8))
         {
             other.gameObject.GetComponent<PlayerController>().TakeDamage(5);
+            anim.SetBool("Explodes", true);
+            Destroy(gameObject, 0.75f);
         }
     }
 }
