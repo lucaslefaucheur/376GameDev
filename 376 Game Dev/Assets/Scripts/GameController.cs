43ed2a411a80a488be8c476dc291a86413a8da04
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2018-11-27 23:15:18 -0500

    - weapon spawn

--- 376 Game Dev/Assets/Scripts/GameController.cs
+++ 376 Game Dev/Assets/Scripts/GameController.cs
@@ -58,7 +58,7 @@ public class GameController : NetworkBehaviour
         {
             GameObject.Find("Manager").GetComponent<MapManagerScript>().spawnChest(new Vector3(-6.94f, -5.61f, 0f));
         }
-        if (numOfPlayers == 4)
+        if (numOfPlayers == 3)
         {
             GameObject.Find("Manager").GetComponent<MapManagerScript>().spawnChest(new Vector3(0.83f, -5.61f, 0f));
         }
