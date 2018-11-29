30527a12f57ac50c741a8731886b4c0986b6427f
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2018-11-28 21:48:52 -0500

    - added information scene

---
+++ 376 Game Dev/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GotoInfoScreen()
+    {
+        SceneManager.LoadScene("Stats");
+    }
+
+}
